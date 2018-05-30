3126e241e2fcdd865fa8690fe5a3ad9e327688f8
xXSnehalXx <snehalmachan@gmail.com>
2018-05-30 18:23:39 +0530

    made changes to the render and setInterval ()

--- test1-app/src/index.js
+++ test1-app/src/index.js
@@ -12,7 +12,9 @@ class Clock extends React.Component {
     //in react no need to mention function to declare a funciton and no need to declare variable just directly this.varName = ?
     //in setInterval the first parament is handler function in which u have to put the function or something you wanna do inside it only
     componentDidMount(){
-        this.timerId = setInterval(() => this.tick() ,1000);
+        this.timerId = setInterval(() =>{
+            return this.tick();
+        },1000);
     }
     componentWillUnmount(){
         clearInterval(this.timerId);
@@ -29,14 +31,14 @@ class Clock extends React.Component {
             <div>
                 <h1>The time</h1>
                 {/* to insert js inside html in react uve to write inside {} */}
-                <h2>{this.state.date.toLocaleTimeString()}</h2>
+                <h2> the time is {this.state.date.toLocaleTimeString()}</h2>
                 <h2>{x}</h2>
             </div> 
         );
     };
 }
-
+let clock = <Clock/>;
 ReactDOM.render(
-    <Clock/>,
+    clock,
     document.getElementById('root')
 );
