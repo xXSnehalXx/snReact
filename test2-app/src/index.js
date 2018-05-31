cae28eee66262e9fc8a82a3212635e7344683efc
xXSnehalXx <snehalmachan@gmail.com>
2018-05-31 11:56:11 +0530

    test2-app update1

--- test2-app/src/index.js
+++ test2-app/src/index.js
@@ -2,3 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class LoggingButton extends React.Component {
+    constructor(props){
+        super(props);
+        // this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick () {
+        let ex = 'Snehal';
+        console.log(`${ex} is in  -> `,this);
+    }
+
+    render(){
+        return(
+            
+            <div>
+                <button onClick={() => this.handleClick()} >Click to see enironment</button>
+            </div>
+        );
+    }
+}
+
+
+
+
+ReactDOM.render(
+<LoggingButton />,
+document.getElementById('root')
+);
