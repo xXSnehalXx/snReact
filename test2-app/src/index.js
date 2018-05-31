23e2b2d68a359f2571f533c59d87b30fb00cfbe3
xXSnehalXx <snehalmachan@gmail.com>
2018-05-31 12:42:59 +0530

    ways of sending parameters to event handlers

--- test2-app/src/index.js
+++ test2-app/src/index.js
@@ -8,29 +8,67 @@ class LoggingButton extends React.Component {
         // this.handleClick = this.handleClick.bind(this);
     }
 
-    handleClick () {
+    handleClick (param) {
+        console.log(param);
         let ex = 'Snehal';
         console.log(`${ex} is in  -> `,this);
     }
 
     render(){
+        let name = this.props.name;
         return(
             
             <div>
-                <button onClick={() => this.handleClick()} >Click to see enironment</button>
+                <button onClick={() => this.handleClick(name)} >Click to see enironment</button>
                 {/* using ()=>this.handleClick() sometimes generates different callbacks whihc is a problem if this is sent as a
                 props to lower component */}
 
                 {/*SO use class fields syntax handleClick = () => {..} or else binding in contructor this.handleClick = this.handleClick.bind(this);*/}
             </div>
+
+        );
+    }
+}
+
+class LoggingButton1 extends React.Component {
+    constructor(props){
+        super(props);
+        // this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick=  (param)=> {
+        console.log(param);
+        let ex = 'Snehal';
+        console.log(`${ex} is in  -> `,this);
+    }
+
+    render(){
+        let name = this.props.name;
+        return(
+            
+            <div>
+                <button onClick={()=>this.handleClick(name)} >Click to see enironment</button>
+                {/* using ()=>this.handleClick() sometimes generates different callbacks whihc is a problem if this is sent as a
+                props to lower component SO avoid this*/}
+
+                {/*SO use class fields syntax handleClick = () => {..} or else binding in contructor this.handleClick = this.handleClick.bind(this);*/}
+
+                {/*to send parameter you can use two ways 
+                
+                1- dont bind in contructor but bind in onClick = this.handleClick.bind(this,prop)
+                   the above binds and sends parameter
+                2- or use classfields syntax and send param using onClick = {this.handleClick(param)}
+
+                */}
+            </div>
+
         );
     }
 }
 
 
 
-
 ReactDOM.render(
-<LoggingButton />,
+<LoggingButton1  name ="snehal"/>,
 document.getElementById('root')
 );
