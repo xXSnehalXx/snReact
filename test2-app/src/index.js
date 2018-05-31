7a33283e0267b2a5125f4d48a805f7eb04ff92db
xXSnehalXx <snehalmachan@gmail.com>
2018-05-31 12:07:31 +0530

    test2-app update2

--- test2-app/src/index.js
+++ test2-app/src/index.js
@@ -18,6 +18,10 @@ class LoggingButton extends React.Component {
             
             <div>
                 <button onClick={() => this.handleClick()} >Click to see enironment</button>
+                {/* using ()=>this.handleClick() sometimes generates different callbacks whihc is a problem if this is sent as a
+                props to lower component */}
+
+                {/*SO use class fields syntax handleClick = () => {..} or else binding in contructor this.handleClick = this.handleClick.bind(this);*/}
             </div>
         );
     }
