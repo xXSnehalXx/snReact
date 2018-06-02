38fee047e9e10ea25c4898435f6681afb4aca26b
xXSnehalXx <snehalmachan@gmail.com>
2018-06-02 15:37:54 +0530

    added 2 files

---
+++ robofriends-app/src/Card.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './index.js'; 
+
+const Card  = (props) => {
+    return (
+        <div className = "bg-light-blue tc br3 pa2 dib ma2 grow shadow-5">
+            <img src={`https://robohash.org/${props.data.id}?200x200`} alt={props.data.username}/>
+            <div>
+                <h2>{props.data.name}</h2>
+                <p>{props.data.email}</p>
+            </div>
+        </div>
+    );
+}
+export {Card};
--- robofriends-app/src/index.js
+++ robofriends-app/src/index.js
@@ -1,7 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Card} from './Card.js';
+import 'tachyons';
+import 'shortid';
+import {robots} from './robots.js';
 
+class App extends React.Component{
+    render(){
+        const cards = robots.map((value,index) => (<Card key={shortid.generate()}  data={value}/>) );
+        return(
+            <div>
+                {cards}
+            </div>
+        );
+    }
+}
 ReactDOM.render(
-    <Card />,
+    <App />,
     document.getElementById('root')
 );
