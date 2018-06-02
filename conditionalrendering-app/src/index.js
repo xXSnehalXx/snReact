2f2a8c1406f50617d2d17b685274d9be78189d41
xXSnehalXx <snehalmachan@gmail.com>
2018-06-02 09:38:11 +0530

    commit 1

--- conditionalrendering-app/src/index.js
+++ conditionalrendering-app/src/index.js
@@ -40,7 +40,7 @@ class LoginControl extends React.Component{
     const isLoggedOn = this.state.isLoggedOn;
     const greeting = isLoggedOn ? (<UserGreeting/>) : (<GuestGreeting/>);
     const button   = isLoggedOn ? (<LogoutButt onClick = {this.handleLogoutClick}/>) : (<LoginButt onClick={this.handleLoginClick}/>);
-    const messsage = isLoggedOn ? (<Mailbox unreadMessages={messages}/>):null;
+    const message = isLoggedOn ? (<Mailbox unreadMessages={messages}/>):null;
         return(
             <div>
                 {greeting}
@@ -72,12 +72,55 @@ function Mailbox(props) {
     );
 }
 
-ReactDOM.render(
-    <LoginControl />,
-    document.getElementById('root')
-);
+// ReactDOM.render(
+//     <LoginControl />,
+//     document.getElementById('root')
+// // );
 
-
-
-
+// class NameForm extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {value: 'mango'};
   
+//       this.handleChange = this.handleChange.bind(this);
+//       this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+  
+//     handleChange(event) {
+//       this.setState({value: event.target.value});
+//     }
+  
+//     handleSubmit(event) {
+//       alert('A name was submitted: ' + this.state.value);
+//       event.preventDefault();
+//     }
+  
+//     render() {
+//       return (
+//         <form onSubmit={this.handleSubmit}>
+//           {/* <label>
+//             Name:
+//             <input type="text" value={this.state.value} onChange={this.handleChange} />
+//             <textarea value={this.state.value} onChange={this.handleChange} />
+//           </label> */}
+//           <label>
+//               select some shit
+//               {/* <select value = {this.state.value} onChange={this.handleChange}>
+//                   <option value="coco">Coconut</option>
+//                   <option value="grape">grape</option>
+//                   <option value="mango">mango</option>
+//                   <option value="orange">orange</option>
+//                   <option value="banana">Banana</option>
+//               </select> */}
+//               <select multiple={true} value={['A','B','C','D']}></select>
+//           </label>
+//           <input type="submit" value="Submit" />
+//         </form>
+//       );
+//     }
+//   }
+
+  ReactDOM.render(
+      <LoginControl />,
+      document.getElementById('root')
+  );
