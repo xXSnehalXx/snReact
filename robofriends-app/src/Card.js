36ea1e0042570f48b48819f32847592c5f794601
xXSnehalXx <snehalmachan@gmail.com>
2018-06-02 15:47:53 +0530

    using props directly

--- robofriends-app/src/Card.js
+++ robofriends-app/src/Card.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'shortid';
 import './index.js'; 
 
 const Card  = (props) => {
--- robofriends-app/src/index.js
+++ robofriends-app/src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Card} from './Card.js';
 import 'tachyons';
-import 'shortid';
 import {robots} from './robots.js';
 
 class App extends React.Component{
     render(){
-        const cards = robots.map((value,index) => (<Card key={shortid.generate()}  data={value}/>) );
+        let shortid = require('shortid');
+        const cards = robots.map((value,index) => <Card key={shortid.generate()}  data={value}/>);
         return(
             <div>
                 {cards}
